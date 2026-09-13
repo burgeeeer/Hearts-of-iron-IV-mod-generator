ab8cd92fa87c69fe18d10e55aafdf20d296e645f
burgeeeer <9wer.00.ty9@gmail.com>
2026-09-13 16:06:46 +0300

    Fix country color generation without overwriting vanilla colors

--- site-extra.js
+++ site-extra.js
@@ -8,6 +8,41 @@
     var counterKey = 'burgeeeer_hoi4_mod_generator_visitors_2026';
     var countApiBase = 'https://countapi.mileshilliard.com/api/v1';
 
+    var vanillaCountryColors = {
+        GER: ['106 119 89', '138 155 116'], ENG: ['201 56 93', '255 73 121'], SOV: ['125 13 24', '163 17 31'],
+        SWE: ['36 132 247', '47 172 255'], FRA: ['57 113 228', '74 147 255'], LUX: ['65 175 179', '85 228 233'],
+        BEL: ['193 171 8', '251 222 10'], HOL: ['203 138 74', '255 179 96'], CZE: ['54 167 156', '70 217 203'],
+        POL: ['197 92 106', '255 120 138'], AUS: ['194 198 215', '255 255 255'], LIT: ['219 219 119', '255 255 155'],
+        EST: ['50 135 175', '99 204 254'], LAT: ['75 77 186', '124 125 184'], SPR: ['242 205 94', '255 255 122'],
+        ITA: ['67 127 63', '87 165 82'], ROM: ['215 196 72', '255 255 120'], YUG: ['72 73 126', '94 95 164'],
+        SWI: ['224 5 5', '193 81 81'], TUR: ['171 190 152', '222 247 198'], GRE: ['93 181 227', '121 235 255'],
+        ALB: ['149 45 102', '194 59 133'], NOR: ['111 71 71', '144 92 92'], DEN: ['153 116 93', '199 151 121'],
+        BUL: ['51 155 0', '210 147 217'], POR: ['39 116 70', '51 151 91'], FIN: ['205 212 228', '255 255 255'],
+        IRE: ['80 159 90', '104 207 117'], HUN: ['249 126 98', '255 164 127'], AFG: ['64 160 167', '83 208 217'],
+        ARG: ['145 157 236', '189 204 255'], AST: ['57 143 97', '74 186 126'], BHU: ['172 122 88', '172 122 88'],
+        BRA: ['76 145 63', '99 189 82'], CAN: ['119 48 39', '155 62 51'], CHL: ['155 101 107', '202 131 139'],
+        COL: ['222 187 91', '255 243 118'], COS: ['152 128 43', '152 128 43'], ECU: ['249 146 98', '255 190 127'],
+        ELS: ['152 130 191', '198 169 248'], ETH: ['152 130 191', '198 169 248'], GUA: ['72 49 112', '72 49 112'],
+        HON: ['128 145 65', '128 145 65'], IRQ: ['178 114 99', '231 148 129'], JAP: ['255 223 179', '255 242 212'],
+        LIB: ['152 130 191', '198 169 248'], MEX: ['104 152 83', '135 198 108'], NIC: ['146 179 191', '146 179 191'],
+        NZL: ['152 130 191', '198 169 248'], PAN: ['152 130 191', '198 169 248'], PER: ['71 113 97', '92 147 126'],
+        PHI: ['152 130 191', '198 169 248'], PRU: ['196 189 204', '255 246 255'], SAF: ['152 130 191', '198 169 248'],
+        SAU: ['171 190 152', '222 247 198'], SIA: ['171 190 152', '222 247 198'], SIK: ['71 190 152', '222 247 198'],
+        TIB: ['80 115 45', '222 247 198'], URG: ['171 190 152', '222 247 198'], VEN: ['171 190 152', '222 247 198'],
+        YUN: ['114 148 80', '222 247 198'], USA: ['20 133 237', '87 160 255'], MON: ['108 140 42', '209 247 133'],
+        MEN: ['185 255 152', '165 230 132'], TAN: ['152 130 191', '198 169 248'], PAR: ['57 113 228', '74 147 255'],
+        CUB: ['140 65 166', '140 65 166'], DOM: ['152 130 191', '198 169 248'], HAI: ['174 113 113', '174 113 113'],
+        YEM: ['155 101 107', '202 131 139'], OMA: ['155 101 107', '202 131 139'], SLO: ['158 161 188', '255 255 255'],
+        RAJ: ['170 10 10', '200 10 10'], CRO: ['230 70 180', '230 255 122'], PRC: ['245 12 55', '178 34 60'],
+        GXC: ['71 113 97', '71 113 97'], SHX: ['82 2 15', '101 30 41'], XSM: ['105 90 132', '105 90 132'],
+        LBA: ['200 180 90', '200 180 90'], EGY: ['230 230 70', '230 230 70'], PAL: ['170 125 80', '170 125 80'],
+        LEB: ['130 150 60', '130 150 60'], JOR: ['111 55 78', '111 55 78'], SYR: ['100 100 150', '100 100 150'],
+        SER: ['160 110 110', '160 110 110'], ICE: ['100 125 175', '100 125 175'], UKR: ['0 80 230', '0 80 230'],
+        AZR: ['69 151 49', '69 151 49'], GEO: ['255 150 150', '255 204 204'], ARM: ['176 102 180', '176 102 180'],
+        LAO: ['176 102 136', '176 102 136'], INS: ['128 158 118', '128 158 118'], VIN: ['230 223 50', '230 223 50'],
+        CAM: ['100 71 150', '100 71 150'], MAL: ['213 169 121', '213 169 121'], MNT: ['77 90 105', '77 90 105']
+    };
+
     window.mkEmptyCountry = function () {
         var c = baseMkEmptyCountry();
         c.color = '';
@@ -116,31 +151,53 @@
 
     var baseGenerateAsync = JSZip.prototype.generateAsync;
     JSZip.prototype.generateAsync = function (options) {
-        try {
-            pullCountriesFromDOM();
-            var modNameEl = document.getElementById('modName');
-            var modName = modNameEl && modNameEl.value.trim() ? modNameEl.value.trim() : 'CustomMod';
-            var modFolder = this.folder(modName);
-            var countryFolder = modFolder.folder('common/countries');
-            var normal = [];
-            var cosmetic = [];
+        pullCountriesFromDOM();
+        var modNameEl = document.getElementById('modName');
+        var modName = modNameEl && modNameEl.value.trim() ? modNameEl.value.trim() : 'CustomMod';
+        var modFolder = this.folder(modName);
+        var countryFolder = modFolder.folder('common/countries');
+        var normal = [];
+        var cosmetic = [];
+        var hasAnyNormalColor = false;
+        var hasAnyCosmeticColor = false;
+        var palette = {};
 
-            for (var i = 0; i < countries.length; i++) {
-                var c = countries[i] || {};
-                var tag = String(c.tag || '').toUpperCase().trim();
-                var rgb = hexToRgb(c.color);
-                if (!tag || !rgb) continue;
-                var block = tag + ' = {\n' +
-                    '    color = rgb { ' + rgb.r + ' ' + rgb.g + ' ' + rgb.b + ' }\n' +
-                    '    color_ui = rgb { ' + rgb.r + ' ' + rgb.g + ' ' + rgb.b + ' }\n' +
-                    '}';
-                if (c.tagType === 'cosmetic') cosmetic.push(block);
-                else normal.push(block);
+        for (var vanillaTag in vanillaCountryColors) {
+            if (Object.prototype.hasOwnProperty.call(vanillaCountryColors, vanillaTag)) {
+                palette[vanillaTag] = vanillaCountryColors[vanillaTag];
             }
+        }
 
-            if (normal.length) countryFolder.file('colors.txt', normal.join('\n\n') + '\n');
-            if (cosmetic.length) countryFolder.file('cosmetic.txt', cosmetic.join('\n\n') + '\n');
-        } catch (e) {}
+        for (var i = 0; i < countries.length; i++) {
+            var c = countries[i] || {};
+            var tag = String(c.tag || '').toUpperCase().trim();
+            var rgb = hexToRgb(c.color);
+            if (!tag || !rgb) continue;
+
+            var rgbText = rgb.r + ' ' + rgb.g + ' ' + rgb.b;
+            if (c.tagType === 'cosmetic') {
+                cosmetic.push(tag + ' = {\n    color = rgb { ' + rgbText + ' }\n    color_ui = rgb { ' + rgbText + ' }\n}');
+                hasAnyCosmeticColor = true;
+            } else {
+                normal.push(tag + ' = {\n    color = rgb { ' + rgbText + ' }\n    color_ui = rgb { ' + rgbText + ' }\n}');
+                palette[tag] = [rgbText, rgbText];
+                hasAnyNormalColor = true;
+            }
+        }
+
+        if (hasAnyNormalColor) {
+            var colorLines = ['#reload countrycolors'];
+            for (var tagKey in palette) {
+                if (!Object.prototype.hasOwnProperty.call(palette, tagKey)) continue;
+                colorLines.push(tagKey + ' = {\n    color = rgb { ' + palette[tagKey][0] + ' }\n    color_ui = rgb { ' + palette[tagKey][1] + ' }\n}');
+            }
+            countryFolder.file('colors.txt', colorLines.join('\n\n') + '\n');
+        }
+
+        if (hasAnyCosmeticColor) {
+            countryFolder.file('cosmetic.txt', ['#reload cosmeticcolors'].concat(cosmetic).join('\n\n') + '\n');
+        }
+
         return baseGenerateAsync.call(this, options);
     };
 
